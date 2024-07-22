f287e4196b2e67acf757f51435c2ff6907c9807f
MayankAgrawal <mayankagrawal0597@gmail.com>
2024-07-22 23:37:07 +0530

    code for the backend

--- job-portal-client/src/Router/Router.jsx
+++ job-portal-client/src/Router/Router.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
+import CreateJob from "../Pages/CreateJob";
+import MyJobs from "../Pages/MyJobs";
+import Login from "../components/Login";
 
 const router = createBrowserRouter([
   {
@@ -12,8 +15,20 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+      {
+        path: "/post-job",
+        element: <CreateJob />,
+      },
+      {
+        path: "/my-jobs",
+        element: <MyJobs />,
+      },
     ],
   },
+  {
+    path:"/login",
+    element: <Login/>
+  }
 ]);
 
 export default router;
